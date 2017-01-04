935a0992286b228212fe1c4a2dea3f2e063289f1
Gebhard Nigsch <gn@guave.ch>
2017-01-04 11:04:59 +0100

    fix inlcude path for cache

--- config/config.php
+++ config/config.php
@@ -11,4 +11,4 @@ $GLOBALS['BE_MOD']['search_console']['search_console'] = array(
     'callback' => 'Guave\SearchConsole\Modules\SearchConsole'
 );
 
-require_once __DIR__.'/search_console_config.php';
+require_once TL_ROOT.'/system/modules/search_console/config/search_console_config.php';
